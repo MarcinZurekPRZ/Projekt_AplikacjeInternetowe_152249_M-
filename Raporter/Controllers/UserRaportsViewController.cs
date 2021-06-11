6201487c4d8050272c6b3e4ca35262f563702418
Marcin Żurek <152249@stud.prz.edu.pl>
2021-06-11 14:06:11 +0200

    Dodanie funkcjonalnosci : Dodawanie raportu

--- Raporter/Controllers/UserRaportsViewController.cs
+++ Raporter/Controllers/UserRaportsViewController.cs
@@ -9,27 +9,49 @@ using Raporter.Models;
 
 
 
+
+
 namespace Raporter.Controllers
 {
     public class UserRaportsViewController : Controller
     {
+        RaporterContext db = new RaporterContext();
         // GET: UserRaportsView
-        public ActionResult ViewRap(UserRaportsViewModel rap)
+        public ActionResult ViewRap()
         {
-            RaporterContext db = new RaporterContext();
+            
 
             var id = Session["UserID"];
             List<Raporty> raportylist = db.Raporties.Where(a => a.UzytkownicyID.ToString() == id.ToString()).ToList();
-            //db.Raporties.ToList();
-            //List <Raporty> raportylist = db.Raporties.Select(raport => new Raporty(){
-            //  UzytkownicyID = 1
-            //}).ToList();
 
             ViewData["RaportyUzytkownika"] =  raportylist ;
                 
 
             return View(ViewData["RaportyUzytkownika"]);
         }
-        
+
+        public ActionResult Create()
+        {
+            ViewBag.UzytkownicyID = new SelectList(db.Uzytkownicies, "UzytkownicyID", "Imie");
+            return View("CreateRap");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RaportyID,DataRaportu,Temat,Tresc,UzytkownicyID")] Raporty raporty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Raporties.Add(raporty);
+                db.SaveChanges();
+                return RedirectToAction("ViewRap", "UserRaportsView");
+                //return RedirectToAction("Index");
+            }
+
+            ViewBag.UzytkownicyID = new SelectList(db.Uzytkownicies, "UzytkownicyID", "Imie", raporty.UzytkownicyID);
+            return View("CreateRap");
+            //return View("CreateRap");
+        }
+
     }
 }
